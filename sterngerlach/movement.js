c69ae71e707fae0a2a2fd6a2be8c067e01832eda
lobber148 <53581397+lobber148@users.noreply.github.com>
2020-06-24 14:47:26 -0400

    Update movement.js
    Depth reading is marginally better now.

--- sterngerlach/movement.js
+++ sterngerlach/movement.js
@@ -164,7 +164,8 @@ function textures() {
 function draw_frames() {
   this.gl = viewport.__renderer.context;
 
-  var vs_source = `
+  var vs_source =
+  `
   precision highp float;
   attribute vec2 vert_pos;
   attribute vec2 a_texcoord;
@@ -173,8 +174,9 @@ function draw_frames() {
     v_texcoord = a_texcoord;
     gl_Position = vec4(vec2(1, -1)*vert_pos, 0, 1);
   }
-  `;
-  var fs_source = `
+  `
+  var fs_source =
+  `
   precision highp float;
   varying vec2 v_texcoord;
   uniform sampler2D image1;
@@ -233,14 +235,15 @@ function draw_frames() {
       // pixel2 = frac*(end-new_start);
     }
 
-    if (depth1<depth2-1.0/255.0) {
+    if (depth1<depth2-0.5/255.0) {
       gl_FragColor = vec4(pixel1, 1);
     } else {
       gl_FragColor = vec4(pixel2, 1);
     }
   }
-  `;
-  var fs2_source = `
+  `
+  var fs2_source =
+  `
   precision highp float;
   varying vec2 v_texcoord;
   uniform sampler2D image1;
@@ -285,7 +288,7 @@ function draw_frames() {
       gl_FragColor = vec4(depth2, 0, 0, 1);
     }
   }
-  `;
+  `
   var prgm = gl.createProgram();
   var vs = gl.createShader(gl.VERTEX_SHADER);
   var fs = gl.createShader(gl.FRAGMENT_SHADER);
@@ -308,6 +311,7 @@ function draw_frames() {
   gl.attachShader(prgm2, fs);
   gl.linkProgram(prgm2);
 
+
   gl.useProgram(prgm);
   gl.uniform1i(gl.getUniformLocation(prgm, "image1"), 0);
   gl.uniform1i(gl.getUniformLocation(prgm, "image2"), 1);
@@ -327,32 +331,42 @@ function draw_frames() {
   gl.uniform1i(gl.getUniformLocation(prgm2, "aliased_image2"), 5);
   var iter2_location = gl.getUniformLocation(prgm2, "iter");
 
-  var vertices = new Float32Array([-1, -1, -1, 1, 1, -1, 1, 1]);
-  var t_vertices = new Float32Array([0, 1, 0, 0, 1, 1, 1, 0]);
+  var vertices = new Float32Array([
+    -1, -1,
+    -1, 1,
+    1, -1,
+    1, 1
+  ]);
+  var t_vertices = new Float32Array([
+    0, 1,
+    0, 0,
+    1, 1,
+    1, 0
+  ]);
 
   var vbo = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-  var pal = gl.getAttribLocation(prgm2, "vert_pos");
+  var pal = gl.getAttribLocation(prgm2, 'vert_pos');
   gl.vertexAttribPointer(
     pal,
     2,
     gl.FLOAT,
     gl.FALSE,
-    2 * Float32Array.BYTES_PER_ELEMENT, // Size of individual vertex
-    0 // Offset
+    2*Float32Array.BYTES_PER_ELEMENT,  // Size of individual vertex
+    0  // Offset
   );
   gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
   gl.enableVertexAttribArray(pal);
   var tbo = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, tbo);
-  var tal = gl.getAttribLocation(prgm2, "a_texcoord");
+  var tal = gl.getAttribLocation(prgm2, 'a_texcoord');
   gl.vertexAttribPointer(
     tal,
     2,
     gl.FLOAT,
     gl.FALSE,
-    2 * Float32Array.BYTES_PER_ELEMENT, // Size of individual vertex
-    0 // Offset
+    2*Float32Array.BYTES_PER_ELEMENT,  // Size of individual vertex
+    0  // Offset
   );
   gl.bufferData(gl.ARRAY_BUFFER, t_vertices, gl.STATIC_DRAW);
   gl.enableVertexAttribArray(tal);
@@ -360,27 +374,27 @@ function draw_frames() {
   gl.useProgram(prgm);
   var vbo = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-  var pal = gl.getAttribLocation(prgm, "vert_pos");
+  var pal = gl.getAttribLocation(prgm, 'vert_pos');
   gl.vertexAttribPointer(
     pal,
     2,
     gl.FLOAT,
     gl.FALSE,
-    2 * Float32Array.BYTES_PER_ELEMENT, // Size of individual vertex
-    0 // Offset
+    2*Float32Array.BYTES_PER_ELEMENT,  // Size of individual vertex
+    0  // Offset
   );
   gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
   gl.enableVertexAttribArray(pal);
   var tbo = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, tbo);
-  var tal = gl.getAttribLocation(prgm, "a_texcoord");
+  var tal = gl.getAttribLocation(prgm, 'a_texcoord');
   gl.vertexAttribPointer(
     tal,
     2,
     gl.FLOAT,
     gl.FALSE,
-    2 * Float32Array.BYTES_PER_ELEMENT, // Size of individual vertex
-    0 // Offset
+    2*Float32Array.BYTES_PER_ELEMENT,  // Size of individual vertex
+    0  // Offset
   );
   gl.bufferData(gl.ARRAY_BUFFER, t_vertices, gl.STATIC_DRAW);
   gl.enableVertexAttribArray(tal);
@@ -396,6 +410,7 @@ function draw_frames() {
   data_texture(gl, depth_in, null, viewport.width, viewport.height);
   var depth_out = gl.createTexture();
   data_texture(gl, depth_out, null, viewport.width, viewport.height);
+
 
   gl.useProgram(prgm2);
   gl.uniform1f(iter2_location, 0);
@@ -414,25 +429,11 @@ function draw_frames() {
   gl.activeTexture(gl.TEXTURE6);
   gl.bindTexture(gl.TEXTURE_2D, depth_out);
   gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
-  gl.framebufferTexture2D(
-    gl.FRAMEBUFFER,
-    gl.COLOR_ATTACHMENT0,
-    gl.TEXTURE_2D,
-    depth_out,
-    0
-  );
+  gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, depth_out, 0);
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-  if (include.length === 2) {
-    this.depth_buffer = new Uint8Array(viewport.width * viewport.height * 4);
-    gl.readPixels(
-      0,
-      0,
-      viewport.width,
-      viewport.height,
-      gl.RGBA,
-      gl.UNSIGNED_BYTE,
-      depth_buffer
-    );
+  if (include.length===2) {
+    this.depth_buffer = new Uint8Array(viewport.width*viewport.height*4);
+    gl.readPixels(0, 0, viewport.width, viewport.height, gl.RGBA, gl.UNSIGNED_BYTE, depth_buffer);
   }
   gl.bindFramebuffer(gl.FRAMEBUFFER, null);
 
@@ -454,30 +455,16 @@ function draw_frames() {
   gl.activeTexture(gl.TEXTURE6);
   gl.bindTexture(gl.TEXTURE_2D, tex_out);
   gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
-  gl.framebufferTexture2D(
-    gl.FRAMEBUFFER,
-    gl.COLOR_ATTACHMENT0,
-    gl.TEXTURE_2D,
-    tex_out,
-    0
-  );
+  gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, tex_out, 0);
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-  if (include.length === 2) {
-    this.buffer = new Uint8Array(viewport.width * viewport.height * 4);
-    gl.readPixels(
-      0,
-      0,
-      viewport.width,
-      viewport.height,
-      gl.RGBA,
-      gl.UNSIGNED_BYTE,
-      buffer
-    );
+  if (include.length===2) {
+    this.buffer = new Uint8Array(viewport.width*viewport.height*4);
+    gl.readPixels(0, 0, viewport.width, viewport.height, gl.RGBA, gl.UNSIGNED_BYTE, buffer);
     draw_final();
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
   } else {
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-    for (var i = 1; i < include.length - 1; i++) {
+    for (var i = 1; i < include.length-1; i++) {
       tex_in = tex_out;
       var tex_out = gl.createTexture();
       data_texture(gl, tex_out, null, viewport.width, viewport.height);
@@ -489,74 +476,40 @@ function draw_frames() {
       gl.activeTexture(gl.TEXTURE0);
       gl.bindTexture(gl.TEXTURE_2D, tex_in);
       gl.activeTexture(gl.TEXTURE1);
-      gl.bindTexture(gl.TEXTURE_2D, images[include[i + 1]].antialiased);
+      gl.bindTexture(gl.TEXTURE_2D, images[include[i+1]].antialiased);
       gl.activeTexture(gl.TEXTURE2);
       gl.bindTexture(gl.TEXTURE_2D, depth_in);
       gl.activeTexture(gl.TEXTURE3);
-      gl.bindTexture(gl.TEXTURE_2D, images[include[i + 1]].depth);
+      gl.bindTexture(gl.TEXTURE_2D, images[include[i+1]].depth);
       gl.activeTexture(gl.TEXTURE4);
       gl.bindTexture(gl.TEXTURE_2D, images[include[i]].aliased);
       gl.activeTexture(gl.TEXTURE5);
-      gl.bindTexture(gl.TEXTURE_2D, images[include[i + 1]].aliased);
+      gl.bindTexture(gl.TEXTURE_2D, images[include[i+1]].aliased);
       gl.activeTexture(gl.TEXTURE6);
       gl.bindTexture(gl.TEXTURE_2D, depth_out);
 
       gl.useProgram(prgm2);
-      gl.uniform1f(iter2_location, i - start);
+      gl.uniform1f(iter2_location, i-start);
       gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
-      gl.framebufferTexture2D(
-        gl.FRAMEBUFFER,
-        gl.COLOR_ATTACHMENT0,
-        gl.TEXTURE_2D,
-        depth_out,
-        0
-      );
+      gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, depth_out, 0);
       gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-      if (i === include.length - 2) {
-        this.depth_buffer = new Uint8Array(
-          viewport.width * viewport.height * 4
-        );
-        gl.readPixels(
-          0,
-          0,
-          viewport.width,
-          viewport.height,
-          gl.RGBA,
-          gl.UNSIGNED_BYTE,
-          depth_buffer
-        );
+      if (i===include.length-2) {
+        this.depth_buffer = new Uint8Array(viewport.width*viewport.height*4);
+        gl.readPixels(0, 0, viewport.width, viewport.height, gl.RGBA, gl.UNSIGNED_BYTE, depth_buffer);
       }
       gl.bindFramebuffer(gl.FRAMEBUFFER, null);
 
       gl.useProgram(prgm);
-      gl.uniform1f(iter_location, i - start);
-      gl.uniform1f(
-        opa_location,
-        ((i - start) / (end - start)) * (1 - start_transparency) +
-          start_transparency
-      );
+      gl.uniform1f(iter_location, i-start);
+      gl.uniform1f(opa_location, (i-start)/(end-start)*(1-start_transparency)+start_transparency);
       gl.activeTexture(gl.TEXTURE6);
       gl.bindTexture(gl.TEXTURE_2D, tex_out);
       gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
-      gl.framebufferTexture2D(
-        gl.FRAMEBUFFER,
-        gl.COLOR_ATTACHMENT0,
-        gl.TEXTURE_2D,
-        tex_out,
-        0
-      );
+      gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, tex_out, 0);
       gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-      if (i === include.length - 2) {
-        this.buffer = new Uint8Array(viewport.width * viewport.height * 4);
-        gl.readPixels(
-          0,
-          0,
-          viewport.width,
-          viewport.height,
-          gl.RGBA,
-          gl.UNSIGNED_BYTE,
-          buffer
-        );
+      if (i===include.length-2) {
+        this.buffer = new Uint8Array(viewport.width*viewport.height*4);
+        gl.readPixels(0, 0, viewport.width, viewport.height, gl.RGBA, gl.UNSIGNED_BYTE, buffer);
         draw_final();
       }
       gl.bindFramebuffer(gl.FRAMEBUFFER, null);
@@ -571,7 +524,8 @@ document.body.appendChild(c);
 var gl2 = c.getContext("webgl");
 
 function draw_final() {
-  var vs_source = `
+  var vs_source =
+  `
   precision highp float;
   attribute vec2 vert_pos;
   attribute vec2 a_texcoord;
@@ -580,8 +534,9 @@ function draw_final() {
     v_texcoord = vec2(a_texcoord.x, 1.0-a_texcoord.y);
     gl_Position = vec4(vert_pos, 0, 1);
   }
-  `;
-  var fs_source = `
+  `
+  var fs_source =
+  `
   precision highp float;
   varying vec2 v_texcoord;
   uniform sampler2D image;
@@ -593,7 +548,7 @@ function draw_final() {
   void main() {
     gl_FragColor = gray(texture2D(image, v_texcoord));
   }
-  `;
+  `
 
   var vs = gl2.createShader(gl2.VERTEX_SHADER);
   var fs = gl2.createShader(gl2.FRAGMENT_SHADER);
@@ -609,32 +564,42 @@ function draw_final() {
 
   gl2.uniform1i(gl2.getUniformLocation(prgm, "image"), 0);
 
-  var vertices = new Float32Array([-1, -1, -1, 1, 1, -1, 1, 1]);
-  var t_vertices = new Float32Array([0, 1, 0, 0, 1, 1, 1, 0]);
+  var vertices = new Float32Array([
+    -1, -1,
+    -1, 1,
+    1, -1,
+    1, 1
+  ]);
+  var t_vertices = new Float32Array([
+    0, 1,
+    0, 0,
+    1, 1,
+    1, 0
+  ]);
 
   var vbo = gl2.createBuffer();
   gl2.bindBuffer(gl2.ARRAY_BUFFER, vbo);
-  var pal = gl2.getAttribLocation(prgm, "vert_pos");
+  var pal = gl2.getAttribLocation(prgm, 'vert_pos');
   gl2.vertexAttribPointer(
     pal,
     2,
     gl2.FLOAT,
     false,
-    2 * Float32Array.BYTES_PER_ELEMENT, // Size of individual vertex
-    0 // Offset
+    2*Float32Array.BYTES_PER_ELEMENT,  // Size of individual vertex
+    0  // Offset
   );
   gl2.bufferData(gl2.ARRAY_BUFFER, vertices, gl2.STATIC_DRAW);
   gl2.enableVertexAttribArray(pal);
   var tbo = gl2.createBuffer();
   gl2.bindBuffer(gl2.ARRAY_BUFFER, tbo);
-  var tal = gl2.getAttribLocation(prgm, "a_texcoord");
+  var tal = gl2.getAttribLocation(prgm, 'a_texcoord');
   gl2.vertexAttribPointer(
     tal,
     2,
     gl2.FLOAT,
     false,
-    2 * Float32Array.BYTES_PER_ELEMENT, // Size of individual vertex
-    0 // Offset
+    2*Float32Array.BYTES_PER_ELEMENT,  // Size of individual vertex
+    0  // Offset
   );
   gl2.bufferData(gl2.ARRAY_BUFFER, t_vertices, gl2.STATIC_DRAW);
   gl2.enableVertexAttribArray(tal);
@@ -650,17 +615,7 @@ function draw_final() {
 
 function data_texture(gl, tex, data, width, height) {
   gl.bindTexture(gl.TEXTURE_2D, tex);
-  gl.texImage2D(
-    gl.TEXTURE_2D,
-    0,
-    gl.RGBA,
-    width,
-    height,
-    0,
-    gl.RGBA,
-    gl.UNSIGNED_BYTE,
-    data
-  );
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, data);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
